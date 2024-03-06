e6e060e6b32fcf83662d4bea37b692d82ba39063
malungu <danielkmalungu@gmail.com>
2024-03-06 12:15:46 +0300

    bugfix: adding wrong defult values

--- airbyte-integrations/connectors/destination-greenplum/destination_greenplum/write.py
+++ airbyte-integrations/connectors/destination-greenplum/destination_greenplum/write.py
@@ -13,8 +13,8 @@ class GreenplumWriter:
         self.database = configs.get("database")
         self.schema = configs.get("schema")
         
-    def greenplum__writer(self, query, values: None) -> None:
-        logging.info(msg=f"Connecting to Greenplum {host}:{port}")
+    def greenplum__writer(self, query, values=None) -> None:
+        logging.info(msg=f"Connecting to Greenplum {self.host}:{self.port}")
         connector = psycopg2.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.database)
         cursor = connector.cursor()
 
@@ -24,29 +24,36 @@ class GreenplumWriter:
         cursor.close()
         connector.close()
         logging.info(msg=f"Objects written to Greenplum {self.host}:{self.port}")
+        
+    def _greenplum_connection(self) -> None:
+        connector = psycopg2.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.database)
+        return connector
 
     def greenplum__writer_insert(self, query, values) -> None:
-        logging.info(msg=f"Connecting to Greenplum {host}:{port}")
-        connector = psycopg2.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.database)
+        logging.info(msg=f"Connecting to Greenplum {self.host}:{self.port}")
+        connector = self._greenplum_connection()
         cursor = connector.cursor()
-
-        cursor.executemany(query=query, vars=values)
+        cursor.executemany(query=query, vars_list=values)
         connector.commit()
         logging.info(msg=f'Sql Executed {query}', exc_info=True)
         cursor.close()
-        connector.close()
         logging.info(msg=f"Objects written to Greenplum {self.host}:{self.port}")
 
     def greenplum__writer_get(self, query) -> None:
-        logging.info(msg=f"Connecting to Greenplum {host}:{port}")
-        connector = psycopg2.connect(host=self.host, port=self.port, user=self.username, password=self.password, database=self.database)
-        cursor = connector.cursor()
-        records = cursor.fetchall(query=query)
-        logging.info(msg=f'Sql Executed {query}', exc_info=True)
-        cursor.close()
-        connector.close()
-        logging.info(msg=f"Objects written to Greenplum {self.host}:{self.port}")
+        try:
+            logging.info(msg=f"Connecting to Greenplum {self.host}:{self.port}")
+            connector = self._greenplum_connection()
+            cursor = connector.cursor()
+            cursor.execute(query=query)
+            records = cursor.fetchall()
+            logging.info(msg=f'Sql Executed {query}', exc_info=True)
+            cursor.close()
+            logging.info(msg=f"Objects written to Greenplum {self.host}:{self.port}")
+        except Exception as e:
+            logging.info(msg=f"An exception occurred: {repr(e)}")
         return records
     
-    # def greenplum__writer_(self, query, values) -> None:
+    def greenplum__connection_close(self) -> None:
+        connector = self._greenplum_connection()
+        connector.close()
 
